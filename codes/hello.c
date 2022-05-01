e68bd783f7e7e21c7666b333809c58d6cf9312e0
阿日哥 <thomas@arigedeMBP.mshome.net>
2022-05-02 00:57:49 +0800

    修改代码块样式

---
+++ codes/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define N 10
+/* Block
+    * comment */
+
+int main()
+{
+    int i;
+
+    // Line comment.
+    puts("Hello world!");
+    
+    for (i = 0; i < N; i++)
+    {
+        puts("LaTeX is also great for programmers!");
+    }
+
+    return 0;
+}
